e77caca3ec7c7e189c3f6600fb453627e830cca2
Julian <juliathouse@aol.com>
2018-05-18 19:52:24 +0200

    Player hinzugefügt

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+   void Start() {
+
+   }
+
+   void Update() {
+      float xVel = 0f;
+      float yVel = 0f;
+
+      if (Input.GetKey(KeyCode.A)) {
+         xVel -= 0;
+      }
+   }
+}
